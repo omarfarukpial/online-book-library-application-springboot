268a73f34c3e93043cd3ff7b9a1c41508f55e9b1
omarfarukpial <omarfarukpial1@gmail.com>
2023-09-09 13:44:19 +0600

    init

---
+++ online-book-library-application/src/test/java/com/pial/onlinebooklibraryapplication/OnlineBookLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pial.onlinebooklibraryapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
